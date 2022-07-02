a771cbf773017111f975ce5943839ef6938fb443
amanda <amandapereiradevcontact@gmail.com>
2022-07-02 16:10:26 -0300

    commit

---
+++ todo.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Tarefa{
+
+    public string $tarefa;
+
+    public string $table;
+
+    
+
+
+    public function __construct($tarefa)
+    {
+        $this->tarefa = $tarefa;
+    }
+
+
+    public function connection(){
+
+
+        $conn = new PDO('');
+    }
+
+    public function insert($table){
+        
+    }
+}
+
+
+
+
+
+?>
